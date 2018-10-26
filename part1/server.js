588e96caf3d3c389f9596812b46ab90ec1066ae9
unknown <lucasromero65@yahoo.com>
2018-10-25 22:38:41 -0500

    hi rucas

--- part1/server.js
+++ part1/server.js
@@ -2,9 +2,29 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.get('/Hello', (req, res)=> {
+  res.send("hello!");
 });
+
+app.post('/create/:id/:name', (req, res)=>{
+  res.json({
+    id: req.params.id,
+    name: req.params.name
+  });
+});
+
+app.get("/", (req, res)=> {
+  res.send(/.part1/.index.html)
+})
+
+app.get("/verify/:age", (req, res)=>{
+  let age = req.params.age
+  if(age > 13){
+    res.sendStatus(200)
+  }else{
+    res.sendStatus(403)
+  }
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,6 +1,44 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+let fs = require("fs")
+
+//one
+
+app.post("/create/:name/:age", (req, res) =>{
+
+  let storage = fs.readFileSync(__dirname + "/storage.json", "utf8")
+    let dataArr = JSON.parse(storage);
+    let obj = ({
+    name: req.params.name,
+    age: req.params.age
+  });
+  dataArr.push(obj);
+  fs.writeFileSync(__dirname + '/storage.json', JSON.stringify(dataArr));
+  res.send("done")
+
+});
+
+//two
+
+app.get("/", (req, res) => {
+  res.send(fs.readFileSync("./storage.json", 'utf8'))
+})
+
+//three
+
+app.get("/:name", (req, res) =>{
+  let name = req.params.name
+  let storage = fs.readFileSync(__dirname + "/storage.json", "utf8")
+  let dataArr = JSON.parse(storage);
+  let results = dataArr.filter(user => user.name == name);
+  res.json(results);
+})
+
+
+
+
+
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
